96f96f4e269548a7bbd79415fc7a7333d16f8ddd
roberthart56 <hart@g.harvard.edu>
2021-07-24 16:40:21 -0400

    Add files via upload

---
+++ organ2021/MIDI_notes/MIDI_Mega.ino
@@ -0,0 +1,40 @@
+const int Numpins=3;
+int pin_array[Numpins]={2,3,4};
+int note_array[Numpins] = {48,49,50};
+bool switch_read[Numpins];
+bool switch_previous[Numpins];
+
+void setup() {
+ Serial.begin(115200);
+ Serial1.begin(31250);  //pins 18 and 19.
+ for(int i=0; i<Numpins; i++) pinMode(pin_array[i], INPUT_PULLUP);
+ for(int i=0; i<Numpins; i++) switch_previous[i] = 1;
+ pinMode(13, OUTPUT);
+}
+
+void MidiSend(int one, int two, int three) {
+  Serial1.write(one);
+  Serial1.write(two);
+  Serial1.write(three);
+  }
+
+void loop() {
+for(int i=0; i<Numpins; i++) switch_read[i]=digitalRead(pin_array[i]);
+//digitalWrite(13, switch_read[0]);  //Test pin2
+
+for(int i=0; i<Numpins; i++){
+  if( switch_read[i] != switch_previous[i]){
+    if( (switch_previous[i]) & !(switch_read[i])){
+       MidiSend(0X90, note_array[i], 0X40);  //send note on to channel 1 at velocity 64
+       Serial.print("Turned on ");
+       Serial.println(note_array[i]);
+    }
+    else {
+      MidiSend(0X80, note_array[i], 0X40);  //send note off to channel 1 at velocity 64
+      Serial.print("Turned off ");
+       Serial.println(note_array[i]);
+    }
+    switch_previous[i] = switch_read[i];
+  }
+}
+}
